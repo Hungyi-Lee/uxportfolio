85586788e158d9c20d0ef4728f773c9cd70bf01e
Hungyi Lee <horngyi6527@gmail.com>
2019-08-27 12:34:33 -0700

    create current_page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
-  resources :works
-  resources :projects
+  resources :works, only: [:new, :edit, :update, :create]
+  resources :projects, only: [:new, :edit, :update, :create]
   get 'about' => 'works#index', :as => :about
   get 'allprojects' => 'projects#index', :as => :all_projects
 
