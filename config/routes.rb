fbf69ffee9f129d2d82cfe844acc69ab10658be1
hungyil14 <hungyi@helpart.co>
2022-10-16 21:30:34 -0700

    remove unneed routes and changed lock

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,20 @@ Rails.application.routes.draw do
   resources :works, only: [:new, :edit, :update, :create]
   resources :projects, only: [:new, :edit, :update, :create]
 
-  resources :sideprojects, only: [:new, :edit, :update, :create, :index]
-  get '/sideprojects/:title', to: "sideprojects#show"
+  # resources :sideprojects, only: [:new, :edit, :update, :create, :index]
+  # get '/sideprojects/:title', to: "sideprojects#show"
 
   # resources :worknotes, only: [:index]
-  get '/blog/careers', to: 'careers#index'
-  get '/blog/skillshares', to: 'skillshares#index'
-  get '/blog/labs', to: 'labs#index'
-  get '/blog/books', to: 'books#index'
+  # get '/blog/careers', to: 'careers#index'
+  # get '/blog/skillshares', to: 'skillshares#index'
+  # get '/blog/labs', to: 'labs#index'
+  # get '/blog/books', to: 'books#index'
 
 
-  get 'about' => 'works#index', :as => :about
+  # get 'about' => 'works#index', :as => :about
   get 'resume' => 'resumes#index', :as => :resume
   # get 'allprojects' => 'projects#index', :as => :all_projects
-  get 'graphicdesign' => 'graphic#index', :as => :graphicdesign
+  # get 'graphicdesign' => 'graphic#index', :as => :graphicdesign
   # resources :projects, only:[:show], param: :title
   get '/projects/:title', to: "projects#show"
 end
